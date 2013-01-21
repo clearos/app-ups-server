7354ab97b57cdcce989b31a03c3c424d65ecd11b
Unknown <unknown@clearfoundation.com>
2013-01-21 03:02:04 +0000

    nut_conf.php pass the source code standards validator.

--- controllers/nut_conf.php
+++ controllers/nut_conf.php
@@ -1,20 +1,81 @@
 <?php
-use \Exception as Exception;
+/**
+ * UPS Server controller.
+ *
+ * @category   Apps
+ * @package    Accounts
+ * @subpackage Controllers
+ * @author     UWS <UWS@Shaw.ca>
+ * @copyright  2011 ClearFoundation
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License version 3 or later
+ * @link       http://www.clearfoundation.com/docs/developer/apps/ups_server/
+ */
+ 
+///////////////////////////////////////////////////////////////////////////////
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+///////////////////////////////////////////////////////////////////////////////
 
-class nut_conf extends ClearOS_Controller
+/**
+ * UPS Server controller.
+ *
+ * @category   Apps
+ * @package    UPS_Server
+ * @subpackage Controllers
+ * @author     UWS <UWS@Shaw.ca>
+ * @copyright  2011 ClearFoundation
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License version 3 or later
+ * @link       http://www.clearfoundation.com/docs/developer/apps/ups_server/
+ */
+class Nut_Conf extends ClearOS_Controller
 {
+    /**
+     * UPS server summary view.
+     *
+     * @return view
+     */
     function index()
     {
         $this->_form('view');
     }
+    
+    /**
+     * Edit NUT Conf entry view.
+     *
+     * @return view
+     */
     function edit()
     {
         $this->_form('edit');
     }
+     /**
+     * UPS server summary view.
+     *
+     * @return view
+     */
     function view()
     {
         $this->_form('view');
     }
+    /**
+     * UPS server entry common add/edit form handler.
+     *
+     * @param string $form_type form type
+     *
+     * @return view
+     */
     function _form($form_type)
     {
         $this->lang->load('ups_server');
